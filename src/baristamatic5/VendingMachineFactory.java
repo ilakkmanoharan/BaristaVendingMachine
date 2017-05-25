4400824c447dc5320d83f2803c7f42b212144dba
ilakkmanoharan <ilakkmanoharan@gmail.com>
2017-05-24 23:06:30 -0500

    Update VendingMachineFactory.java

--- src/baristamatic5/VendingMachineFactory.java
+++ src/baristamatic5/VendingMachineFactory.java
@@ -1,5 +1,9 @@
 package baristamatic5;
 
+/* Copyright (C) 2017 Ilakkuvaselvi Manoharan - All Rights Reserved
+ * 
+ */
+
 /**
  * Factory class to create instance of Vending Machine, this can be extended to create instance of
  * different types of vending machines.
