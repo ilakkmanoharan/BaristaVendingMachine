07d6b0087f43eab7189894a457771ecc3fcb21a2
ilakkmanoharan <ilakkmanoharan@gmail.com>
2017-05-24 23:04:01 -0500

    Update Ingredients.java

--- src/baristamatic5/Ingredients.java
+++ src/baristamatic5/Ingredients.java
@@ -1,5 +1,8 @@
 package baristamatic5;
 
+/* Copyright (C) 2017 Ilakkuvaselvi Manoharan - All Rights Reserved
+ * 
+ */
 public enum Ingredients {
 	
 	COCOA("Cocoa", 0.90), COFFEE("Coffee", 0.75), CREAM("Cream", 0.25), DECAFCOFFEE("Decaf Coffee", 0.75), ESPRESSO("Espresso", 1.10), FOAMMILK("Foamed Milk", 0.35), STEAMMILK("Steam Milk", 0.35), SUGAR("Sugar", 0.25), WHIPCREAM("Whipped Cream", 1.00);
